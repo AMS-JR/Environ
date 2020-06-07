44a46a933654bd3ef466023911e4c5c9a1741137
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-07 14:34:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?
+class AMADOU{
+  private $test = getenv('TEST');
+  private $API = getenv('API');
+public function env(){
+ echo $this->test." and ".$this->API;
+}
+}
 echo "HELLLO AMADOU SARJO JALLOW";
-echo getenv('TEST');
-echo getenv('API');
+$RES = new AMADOU();
+$RES->env();
