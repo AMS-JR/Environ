ed5454b58107eeda87bc1b5c2f9331d2578ebecc
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-07 15:27:36 +0000

    Update Amadou.php

--- account/Amadou.php
+++ account/Amadou.php
@@ -1,11 +1,12 @@
 <?php
 define('TEST',  getenv('TEST'));
 define('API', getenv('API'));
+define('PASSWORD',  getenv('PASSWORD'));
 class Amadou{
 //    const TEST = getenv('TEST');
 //    const API = getenv('API');
 public function env(){
- echo TEST." and ".API;
+ echo TEST." and ".API." pwd: ".PASSWORD;
    }
  }
 ?>
