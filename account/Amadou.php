97fbe13bdb955fdb85967fd5d761830022287fb2
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2020-06-07 14:43:18 +0000

    Update Amadou.php

--- account/Amadou.php
+++ account/Amadou.php
@@ -1,7 +1,7 @@
 <?php
 class Amadou{
-  private $test = getenv('TEST');
-  private $API = getenv('API');
+   $test = getenv('TEST');
+   $API = getenv('API');
 public function env(){
  echo $this->test." and ".$this->API;
    }
